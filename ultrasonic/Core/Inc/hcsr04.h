c0a5b2be25f45fbf5a54b8e5744255e4a5fd2a2c
Mateusz <rzeszutko98@gmail.com>
2021-03-22 19:07:47 +0100

    Implemenation of functions for the operation of the ultrasonic sensor

---
+++ ultrasonic/Core/Inc/hcsr04.h
@@ -0,0 +1,18 @@
+/*
+ * hcsr04.h
+ *
+ *  Created on: Mar 7, 2021
+ *      Author: rzesz
+ */
+
+#ifndef INC_HCSR04_H_
+#define INC_HCSR04_H_
+
+
+#define HCSR04_PWM_CHANNEL TIM_CHANNEL_3
+#define HCSR04_START_CHANNEL TIM_CHANNEL_1
+#define HCSR04_STOP_CHANNEL TIM_CHANNEL_2
+
+
+
+#endif /* INC_HCSR04_H_ */
