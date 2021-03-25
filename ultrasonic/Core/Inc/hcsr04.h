2c69e16c1648b25fa8a07493f13c6ffa82e8769a
Mateusz <rzeszutko98@gmail.com>
2021-03-26 00:03:43 +0100

    Low Power Mode -- Sleep Mode

--- ultrasonic/Core/Inc/hcsr04.h
+++ ultrasonic/Core/Inc/hcsr04.h
@@ -2,17 +2,16 @@
  * hcsr04.h
  *
  *  Created on: Mar 7, 2021
- *      Author: rzesz
+ *      Author: rzeszutko
  */
 
 #ifndef INC_HCSR04_H_
 #define INC_HCSR04_H_
 
-
 #define HCSR04_PWM_CHANNEL TIM_CHANNEL_3
 #define HCSR04_START_CHANNEL TIM_CHANNEL_1
 #define HCSR04_STOP_CHANNEL TIM_CHANNEL_2
 
-
+/* TO DO: Kalman fiter  */
 
 #endif /* INC_HCSR04_H_ */
